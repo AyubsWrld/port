9aa33a8750e5b3af63772f4fdcd7e5b7ba4eeb22
0xtrensetta <ayubfmohammed@gmail.com>
2025-10-14 20:12:30 -0600

    middleware work

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: '/capocantcode.io/',
-  base: '/'
+  base: '/capocantcode.io/',
+  //base: '/'
 })
